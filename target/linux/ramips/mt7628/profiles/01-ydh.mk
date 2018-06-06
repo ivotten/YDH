2a423b4db7ebbfcb00a0163f5a3a0c8dbc210d42
ivotten <votten51@gmail.com>
2018-06-06 12:09:39 +0800

    ADD More Devices Support

--- target/linux/ramips/mt7628/profiles/01-ydh.mk
+++ target/linux/ramips/mt7628/profiles/01-ydh.mk
@@ -5,12 +5,12 @@
 # See /LICENSE for more information.
 #
 
-define Profile/YDH
-	NAME:=YDH iot Board
+define Profile/YDH_64M_8
+	NAME:=YDH Kit 64MByte 8MByte
 	PACKAGES:=\
 		kmod-usb-core kmod-usb2 kmod-usb-ohci \
 		uboot-envtools kmod-ledtrig-netdev \
-  	mountd \
+		mountd \
 		uhttpd rpcd rpcd-mod-iwinfo \
 		rpcd-mod-rpcsys cgi-io spi-tools \
 		kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 block-mount e2fsprogs \
@@ -18,11 +18,105 @@ define Profile/YDH
 		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
 		kmod-usb-storage \
 		kmod-sound-core kmod-sound-mtk madplay-alsa alsa-utils \
-		YDH-wifi \
-    shairport_mmap
+		YDH_MTK_WiFi \
+		shairport_mmap
 endef
 
-define Profile/YDH/Description
+define Profile/YDH_64M_8/Description
 	YDH iot Board base packages.
 endef
-$(eval $(call Profile,YDH))
+$(eval $(call Profile,YDH_64M_8))
+
+
+define Profile/YDH_128M_16
+	NAME:=YDH Kit 128MByte 16MByte
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		uboot-envtools kmod-ledtrig-netdev \
+		mountd \
+		uhttpd rpcd rpcd-mod-iwinfo \
+		rpcd-mod-rpcsys cgi-io spi-tools \
+		kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 block-mount e2fsprogs \
+		kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-sound-core kmod-sound-mtk madplay-alsa alsa-utils \
+		YDH_MTK_WiFi \
+		shairport_mmap
+endef
+
+define Profile/YDH_128M_16/Description
+	YDH iot Board base packages.
+endef
+$(eval $(call Profile,YDH_128M_16))
+
+
+
+define Profile/YDH_128M_32
+	NAME:=YDH Kit 128MByte 32MByte
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		uboot-envtools kmod-ledtrig-netdev \
+		mountd \
+		uhttpd rpcd rpcd-mod-iwinfo \
+		rpcd-mod-rpcsys cgi-io spi-tools \
+		kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 block-mount e2fsprogs \
+		kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-sound-core kmod-sound-mtk madplay-alsa alsa-utils \
+		YDH_MTK_WiFi \
+		shairport_mmap
+endef
+
+define Profile/YDH_128M_32/Description
+	YDH iot Board base packages.
+endef
+$(eval $(call Profile,YDH_128M_32))
+
+
+define Profile/YDH_256M_16
+	NAME:=YDH Kit 256MByte 16MByte
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		uboot-envtools kmod-ledtrig-netdev \
+		mountd \
+		uhttpd rpcd rpcd-mod-iwinfo \
+		rpcd-mod-rpcsys cgi-io spi-tools \
+		kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 block-mount e2fsprogs \
+		kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-sound-core kmod-sound-mtk madplay-alsa alsa-utils \
+		YDH_MTK_WiFi \
+		shairport_mmap
+endef
+
+define Profile/YDH_256M_16/Description
+	YDH iot Board base packages.
+endef
+$(eval $(call Profile,YDH_256M_16))
+
+
+define Profile/YDH_256M_32
+	NAME:=YDH Kit 256MByte 32MByte
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		uboot-envtools kmod-ledtrig-netdev \
+		mountd \
+		uhttpd rpcd rpcd-mod-iwinfo \
+		rpcd-mod-rpcsys cgi-io spi-tools \
+		kmod-fs-vfat kmod-fs-exfat kmod-fs-ext4 block-mount e2fsprogs \
+		kmod-i2c-core kmod-i2c-ralink \
+		kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+		kmod-usb-storage \
+		kmod-sound-core kmod-sound-mtk madplay-alsa alsa-utils \
+		YDH_MTK_WiFi \
+		shairport_mmap
+endef
+
+define Profile/YDH_256M_32/Description
+	YDH iot Board base packages.
+endef
+$(eval $(call Profile,YDH_256M_32))
+
